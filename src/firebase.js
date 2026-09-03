441a2a399a583a124531a89dfdd131a9d96b58f4
Sudo-Anu <anuragraut551@gmail.com>
2026-09-03 15:52:44 +0530

    fix: hardcode firebase keys for gh-pages

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBuqTCdnrzWvZXZKmaeG4hpJbHDrxG20rs",
+  authDomain: "portfolio-ed72a.firebaseapp.com",
+  projectId: "portfolio-ed72a",
+  storageBucket: "portfolio-ed72a.firebasestorage.app",
+  messagingSenderId: "906680679054",
+  appId: "1:906680679054:web:cd3914c337d8b1c21f27df",
+  measurementId: "G-YR41FBKKJ7"
 };
 
 const app = initializeApp(firebaseConfig);
